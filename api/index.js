dc13313346cfe8cc77a52c6ae68030855aa1223c
kiyoaki0218 <maruyamakiyoaki@gmail.com>
2026-06-21 16:52:46 +0900

    Fix database import path in api/index.js

--- api/index.js
+++ api/index.js
@@ -1,9 +1,9 @@
-﻿const express = require('express');
+const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const nacl = require('tweetnacl');
 const naclUtil = require('tweetnacl-util');
-const db = require('../db');
+const db = require('./db');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
